576a4aa8c0199e0c8cf3ea7f9c7de06065e7379e
luciaucalgaary <160363637+luciaucalgaary@users.noreply.github.com>
2024-02-17 16:57:54 -0700

    Add files via upload

--- Player.gd
+++ Player.gd
@@ -1,26 +1,29 @@
 extends Area2D
 
-@export var speed = 1  #(pixels/sec)
+@export var speed = 100  #(pixels/sec)
 var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	
 func _process(delta):
 	var velocity = Vector2.ZERO #The player's movement vector???
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	if Input.is_action_just_pressed("move_left"):
+	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-	if Input.is_action_just_pressed("move_up"):
-		velocity.y += 1
-	if Input.is_action_just_pressed("move_down"):
+	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
 	else:
-		$AnimatedSprite2D.stop()
+		velocity = Vector2.ZERO
+		#$AnimatedSprite2D.stop()
+		
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 
