96a605ef3ae00ad16e7963e07ef7a26466f6de2e
GraceLanBM <46943231+GraceLanBM@users.noreply.github.com>
2024-02-17 19:36:33 -0700

    Update rock generate (not done)

--- Rock.gd
+++ Rock.gd
@@ -1,20 +1,34 @@
-extends StaticBody2D
+extends RigidBody2D
 
-var rock_pos = PackedVector2Array()
+#var time = 0
+#var rock_pos = PackedVector2Array()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	"""
 	var rock_img = $AnimatedSprite2D.sprite_frames.get_animation_names()
+	$AnimatedSprite2D.play(rock_img[0])
+	"""
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+	"""
 	var is_rock = true
-	while is_rock:
-		var shadow_posX = randi_range(0, 750)
-		var shadow_posY = randi_range(100, 550)
-		if rock_pos.has(Vector2(shadow_posX, shadow_posY)):
-			rock_pos.append(Vector2(shadow_posX, shadow_posY))
-			is_rock = false
+	# generate a new position for new rocks
+	var shadow_posX = randi_range(0, 750)
+	var shadow_posY = randi_range(100, 550)
+	# check if the position already have a rock
+	if rock_pos.has(Vector2(shadow_posX, shadow_posY)):
+		# debug info
+		print("x=", shadow_posX, " y=", shadow_posY)
+		# set the rock image to the new position
+		self.position = Vector2(shadow_posX, shadow_posY)
+		rock_pos.append(Vector2(shadow_posX, shadow_posY))
+		is_rock = false
+		
 
 func _on_visible_screen_notifer_2d_screen_exited():
-	
+	queue_free()
+	"""
---
+++ main.gd
@@ -0,0 +1,30 @@
+extends Node
+
+var rock_pos = PackedVector2Array()
+var shadow_x
+var shadow_y
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#var player = preload("res://player.tscn").instance()
+	#player.position = Vector2(400, 350)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_RockTimer():
+	var shadow = preload("res://rock.tscn").instance()
+	while true:
+		# generate new rock position
+		shadow_x = randi_range(0, 750)
+		shadow_y = randi_range(100, 550)
+		# debug info
+		print("x=", shadow_x, "y=", shadow_y)
+		# check if the position not has a rock
+		if not rock_pos.has(Vector2(shadow_x, shadow_y)):
+			# set the shadow to new position
+			shadow.position = Vector2(shadow_x, shadow_y)	
+			rock_pos.append(Vector2(shadow_x, shadow_y))	
+			add_child(shadow)
+			break
+	$RockTimer.wait_time = 5
+	
