fa07537dee4236400d16b593830fe09a879c0841
GraceLanBM <gracelanblue@gmail.com>
2024-02-17 20:35:36 -0700

    Manage the folder

--- Global.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-#Global variables for updating between the game and the Gameover scenes
-# Boolean to update true when player dies
-var game_over = false
-#updates from timer script
-var time
-#needs to update the score when player dies to show up on game over
-var score
